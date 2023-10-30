e187847c9e2305be8c89cfb40b4ba72e5e520bec
Donschki <donvonkrog@gmail.com>
2023-10-30 20:34:53 +0200

    a

--- Js/index.js
+++ Js/index.js
@@ -5,7 +5,7 @@
 let hamburger = document.querySelector (".hamburgerMenu");
 let sideBar = document.querySelector (".sideBar");
 
-//                          TOGGLE SIDE BAR
+// TOGGLE SIDE BAR
 
 hamburger.addEventListener("click", () => {
     sideBar.classList.toggle("change");
@@ -13,26 +13,25 @@ hamburger.addEventListener("click", () => {
     
 });
 
-//                          CLICK TO REMOVE SIDE BAR
+// //CLICK TO REMOVE SIDE BAR
 
+// document.querySelector('.one').addEventListener('click', () => {
+//   sideBar.classList.remove('change');
+// });
 
-document.querySelector('.one').addEventListener('click', () => {
-  sideBar.classList.remove('change');
-});
+// document.querySelector('.two').addEventListener('click', () => {
+//   sideBar.classList.remove('change');
+// });
 
-document.querySelector('.two').addEventListener('click', () => {
-  sideBar.classList.remove('change');
-});
+// //PROJECT SECTION -> CONTAINER ANIMATE ON SCROLL 
 
-//PROJECT SECTION -> CONTAINER ANIMATE ON SCROLL 
+// const projectContainer = document.querySelectorAll('.project-container');
 
-const projectContainer = document.querySelectorAll('.project-container');
+// const whiteWash = document.getElementsByClassName ('white-wash-animation');
 
-const whiteWash = document.getElementsByClassName ('white-wash-animation');
-
-projectContainer.addEventListener('click', function(){
-   whiteWash.classList.toggle ('')
-});
+// projectContainer.addEventListener('click', function(){
+//    whiteWash.classList.toggle ('')
+// });
 
 
 
